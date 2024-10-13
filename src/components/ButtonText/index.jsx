d816bf6b97f3c26e62b5bcb88a4bdc42fed83cf7
arauujo <wesleey988@gmail.com>
2024-10-13 20:03:56 -0300

    added children to component to accept icon

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,8 +1,9 @@
 import { Container } from './styles';
 
-export function ButtonText({ title, isActive = false, ...rest }) {
+export function ButtonText({ title, isActive = false, children, ...rest }) {
   return (
     <Container type="button" $isactive={isActive.toString()} {...rest}>
+      {children}
       {title}
     </Container>
   );
