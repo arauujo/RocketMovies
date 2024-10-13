91049f6867e78eb2a0d917fc379e4a9d5bcae4c7
arauujo <wesleey988@gmail.com>
2024-10-13 17:24:51 -0300

    added AuthProvider at main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
-
+import { AuthProvider } from './hooks/auth';
 import { Routes } from './routes';
-
+import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 );
