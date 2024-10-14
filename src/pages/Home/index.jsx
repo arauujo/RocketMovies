6f4b57547ebfb6ff574cc90b0bbb0a2c24269a7d
arauujo <wesleey988@gmail.com>
2024-10-14 00:20:55 -0300

    added function to show and search movies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,36 +1,55 @@
-import { Container, Content, Button } from './styles.jsx';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
 import { FiPlus } from 'react-icons/fi';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { Movie } from '../../components/Movie';
-import { Link } from 'react-router-dom';
+import { Container, Content, Button } from './styles.jsx';
 
 export function Home() {
+  const [search, setSearch] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const navigate = useNavigate();
+
+  function handleAddMovie () {
+    navigate('/new');
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
+  }
+
+  function handleSearchChange(event) {
+    setSearch(event.target.value);
+  }
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await api.get(
+        `/movie_notes?title=${search}`
+      );
+      setMovies(response.data);
+    }
+    fetchMovies();
+  }, [search]);
+
   return (
     <Container>
-      <Header />
+      <Header onSearchChange={handleSearchChange} />
 
       <Content>
         <Section title="Meus filmes">
-          <Link to="/new">
-            <Button title="Adicionar filme" icon={FiPlus} />
-          </Link>
+          <Button title="Adicionar filme" icon={FiPlus} onClick={handleAddMovie} />
 
-          <Link to="/details/:id">
+          {movies.map(movie => (
             <Movie
-              data={{
-                title: 'Interestellar',
-                presentation: `Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.
-
-                    Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.`,
-                tags: [
-                  { id: '1', name: 'Ficção Científica' },
-                  { id: '2', name: 'Drama' },
-                  { id: '3', name: 'Família' },
-                ],
-              }}
+              key={String(movie.id)}
+              data={movie}
+              onClick={() => handleDetails(movie.id)}
             />
-          </Link>
+          ))}
         </Section>
       </Content>
     </Container>
