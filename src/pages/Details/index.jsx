bb821977155a359fdef59ce1855d9b718977fa5c
arauujo <wesleey988@gmail.com>
2024-10-16 00:29:14 -0300

    created get movie data function

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,23 +1,54 @@
-import { Container, Content } from './styles';
+import { useEffect, useState, useRef } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { FiArrowLeft, FiStar, FiClock } from 'react-icons/fi';
+import { api } from '../../services/api';
+import { Loader } from '../../components/Loader';
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Tag } from '../../components/Tag';
+import { Container, Content } from './styles';
 
 export function Details() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const hasFetchedData = useRef(false);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  useEffect(() => {
+    if (hasFetchedData.current) return; // Se já buscou os dados não executa novamente
+    hasFetchedData.current = true; // Marca que os dados já foram buscados para evitar chamadas futuras
+    async function fetchMovies() {
+      try {
+        const response = await api.get(`/movie_notes/${params.id}`);
+        setData(response.data);
+        setIsLoading(false); // Atualiza o estado de carregamento para falso quando o componente for montado
+      } catch (error) {
+        alert("Erro ao buscar os dados:", error);
+      }
+    }
+
+    fetchMovies();
+  }, [params.id]);
+
+  if (isLoading) return <Loader />
+
   return (
     <Container>
       <Header />
 
       <main>
         <Content>
-          <a href="/">
+          <ButtonText isActive title="Voltar" onClick={handleBack}>
             <FiArrowLeft />
-            <ButtonText isActive title="Voltar" />
-          </a>
+          </ButtonText>
 
           <header>
-            <h1>Interestellar</h1>
+            <h1>{data.title}</h1>
 
             <div className="ratings">
               <FiStar />
@@ -29,50 +60,21 @@ export function Details() {
           </header>
 
           <div className="author-info">
-            <img src="https://www.github.com/arauujo.png" alt="Imagem de" />
-            <span>Por Wesley Araujo</span>
+            <img src={data.userData.avatar} alt={`Imagem de ${data.userData.name}`} />
+            <span>Por {data.userData.name}</span>
             <FiClock />
-            <span>23/05/22 às 08:00</span>
+            <span>{data.created_at}</span>
           </div>
 
-          <section>
-            <Tag title="Ficção Científica" />
-            <Tag title="Drama" />
-            <Tag title="Família" />
-          </section>
+          {data.movieTags && (
+            <section>
+              {data.movieTags.map(movieTag => (
+                <Tag key={String(movieTag.id)} title={movieTag.name} />
+              ))}
+            </section>
+          )}
 
-          <p>
-            Pragas nas colheitas fizeram a civilização humana regredir para uma
-            sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto
-            da NASA, tem uma fazenda com sua família. Murphy, a filha de dez
-            anos de Cooper, acredita que seu quarto está assombrado por um
-            fantasma que tenta se comunicar com ela. Pai e filha descobrem que o
-            "fantasma" é uma inteligência desconhecida que está enviando
-            mensagens codificadas através de radiação gravitacional, deixando
-            coordenadas em binário que os levam até uma instalação secreta da
-            NASA liderada pelo professor John Brand. O cientista revela que um
-            buraco de minhoca foi aberto perto de Saturno e que ele leva a
-            planetas que podem oferecer condições de sobrevivência para a
-            espécie humana. As "missões Lázaro" enviadas anos antes
-            identificaram três planetas potencialmente habitáveis orbitando o
-            buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em
-            homenagem aos astronautas que os pesquisaram. Brand recruta Cooper
-            para pilotar a nave espacial Endurance e recuperar os dados dos
-            astronautas; se um dos planetas se mostrar habitável, a humanidade
-            irá seguir para ele na instalação da NASA, que é na realidade uma
-            enorme estação espacial. A partida de Cooper devasta Murphy. Além de
-            Cooper, a tripulação da Endurance é formada pela bióloga Amelia,
-            filha de Brand; o cientista Romilly, o físico planetário Doyle, além
-            dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem
-            a Miller, porém descobrem que o planeta possui enorme dilatação
-            gravitacional temporal por estar tão perto de Gargântua: cada hora
-            na superfície equivale a sete anos na Terra. Eles entram em Miller e
-            descobrem que é inóspito já que é coberto por um oceano raso e
-            agitado por ondas enormes. Uma onda atinge a tripulação enquanto
-            Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando
-            a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem
-            que 23 anos se passaram.
-          </p>
+          <p>{data.description}</p>
         </Content>
       </main>
     </Container>
