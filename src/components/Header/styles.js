dcdb74d7eb03c699a9ea300c2b7900793aa7e317
arauujo <wesleey988@gmail.com>
2024-10-19 18:15:06 -0300

    adjusted profile component style

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Container = styled.header`
@@ -18,7 +19,7 @@ export const Container = styled.header`
   }
 `;
 
-export const Profile = styled.div`
+export const Profile = styled(Link)`
   display: flex;
   align-items: center;
 
