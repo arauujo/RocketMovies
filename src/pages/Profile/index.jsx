5c1bcd1f493a2518968cc08ccefc6170c63553e0
arauujo <wesleey988@gmail.com>
2024-10-19 18:17:30 -0300

    created functions for update profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,40 +1,102 @@
-import { Container, Form, Avatar } from './styles';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { api } from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+import { Container, Form, Avatar } from './styles';
 
 export function Profile() {
+  const { user, updateProfile } = useAuth();
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  async function handleUpdate() {
+    const updated = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld,
+    };
+    const userUpdated = Object.assign(user, updated);
+
+    await updateProfile({ user: userUpdated, avatarFile });
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
+  }
+
   return (
     <Container>
       <header>
-        <Link to="/">
+        <ButtonText isActive title="Voltar" onClick={handleBack}>
           <FiArrowLeft />
-          <ButtonText isActive title="Voltar" />
-        </Link>
+        </ButtonText>
       </header>
 
       <Form>
         <Avatar>
-          <img src="https://github.com/arauujo.png" alt="Foto do usuário" />
+          <img src={avatarUrl} alt={`Foto de ${name}`} />
 
           <label htmlFor="avatar">
             <FiCamera />
 
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
 
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
+        />
 
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
+        />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   );
