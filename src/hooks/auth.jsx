0c9808dbe32dfd88145523f6471d0b3c2c473b20
arauujo <wesleey988@gmail.com>
2024-10-19 18:16:12 -0300

    created update profile function

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -33,6 +33,30 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user, avatarFile }) {
+    try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append('avatar', avatarFile);
+
+        const response = await api.patch('/users/avatar', fileUploadForm);
+        user.avatar = response.data.avatar;
+      }
+
+      await api.put('/users', user);
+      localStorage.setItem('@rocketmovies:user', JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert('Perfil atualizado!');
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert('Não foi possível atualizar o perfil!');
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('@rocketmovies:token');
     const user = localStorage.getItem('@rocketmovies:user');
@@ -48,7 +72,7 @@ function AuthProvider({ children }) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, signOut, updateProfile, user: data.user }}>
       {children}
     </AuthContext.Provider>
   );
